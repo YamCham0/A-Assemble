966bd4684cd71b6be5e63f0c687c1908bf4f7b1f
Yamil Chamo <yajochakha@gmail.com>
2021-11-19 20:07:51 -0600

    It's alive!

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const managerData = [
         message: "What's your manager email address?"
     },
     {
-        name: "oNumber",
+        name: "office",
         type: "input",
         message: "What's your manager office number?"
     },
@@ -35,12 +35,12 @@ const managerData = [
 
 const engineersData = [
     {
-        name: "eName",
+        name: "name",
         type: "input",
         message: "What's your engineer name?"
     },
     {
-        name: "eId",
+        name: "id",
         type: "input",
         message: "What's your engineer employee ID?"
     },
@@ -58,12 +58,12 @@ const engineersData = [
 
 const internsData = [
     {
-        name: "iName",
+        name: "name",
         type: "input",
         message: "What's your intern name?"
     },
     {
-        name: "iId",
+        name: "id",
         type: "input",
         message: "What's your intern employee ID?"
     },
@@ -127,8 +127,7 @@ function userChoices () {
         internPrompt ();
     }else{
         console.log("You're done!")
-        generateHTML();
-        // createFile();
+        imDone();
     }
     })
 };
@@ -145,46 +144,30 @@ function engineersPrompt () {
 .prompt(engineersData)
 .then((answers) => {
     console.log(answers);
-    let engineer = new Engineer(answers.eName, answers.eId, answers.email, answers.gitHub);
-    eTeam.push(engineer);
+    let engineer = new Engineer(answers.name, answers.id, answers.email, answers.gitHub);
+    const engineerPage = generateEngineerPage(engineer);
+    fs.appendFile('./dist/index.html', engineerPage,(err) => 
+    err ? console.log(err) : console.log('Yay!')
+    );
     userChoices();
     })
 };
 
-
-function internPrompt () {
-    inquirer
-.prompt(internsData)
-.then((answers) => {
-    console.log(answers);
-    let intern = new Intern(answers.iName, answers.iId, answers.email, answers.school);
-    eTeam.push(intern);
-    userChoices();
-    })
-};
-
-function init() {
-inquirer
-.prompt(managerData)
-.then((answers) => {
-    console.log(answers);
-    let manager = new Manager(answers.mName, answers.mId, answers.email, answers.oNumber); 
-    eTeam.push(manager);
-    userChoices();
-    })
-
-};
-// 3 separate fuctions to return 3 Strings
-// generatemanager card, index0
-// generate engineer and intern
+const generateEngineerPage = (engineer) => 
+`<div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
+  <div class="card-header">Engineer:${engineer.name}</div>
+  <div class="card-body">
+    <h5 class="card-title">Id:${engineer.id}</h5>
+    <p class="card-text">Email:<a href="mailto:${engineer.email}"> ${engineer.email}</a></p>
+    <p class="card-text">Github:${engineer.github}</p>
+  </div>
+</div>`
 
 
 
+const generateManagerPage = (manager) =>
 
-
-function generateHTML(data, name, id, email, github, school, oNumber) {
-
-    return ` 
+    ` 
     <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -195,25 +178,103 @@ function generateHTML(data, name, id, email, github, school, oNumber) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    
-<div class="card" style="width: 18rem;">
-    <div class="card-body">
-        <h5 class="card-title"></h5>
-        <p class="card-text"></p>
-    </div>
-    <ul class="list-group list-group-flush">
-        <li class="list-group-item">An item</li>
-        <li class="list-group-item">A second item</li>
-        <li class="list-group-item">A third item</li>
-    </ul>
-</div>
+<div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
+  <div class="card-header">Manager:${manager.name}</div>
+  <div class="card-body">
+    <h5 class="card-title">Id:${manager.id}</h5>
+    <p class="card-text">Email:<a href="mailto:${manager.email}"> ${manager.email}</a></p>
+    <p class="card-text">Office Number:${manager.office}</p>
+  </div>
+</div>`
 
-</body>
+
+
+function internPrompt () {
+    inquirer
+.prompt(internsData)
+.then((answers) => {
+    console.log(answers);
+    let intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+    const interPage = generateInternPage(intern);
+    fs.appendFile('./dist/index.html', interPage,(err, results) => 
+    err ? console.log(err) : console.log('Yay!')
+    );
+    userChoices();
+    })
+};
+const generateInternPage = (intern) => 
+`<div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
+  <div class="card-header">Intern:${intern.name}</div>
+  <div class="card-body">
+    <h5 class="card-title">Id:${intern.id}</h5>
+    <p class="card-text">Email:<a href="mailto:${intern.email}"> ${intern.email}</a></p>
+    <p class="card-text">School:${intern.school}</p>
+  </div>
+</div>`
+
+function init() {
+inquirer
+.prompt(managerData)
+.then((answers) => {
+    console.log(answers);
+    console.log(answers.office);
+    let manager = new Manager(answers.mName, answers.mId, answers.email, answers.office);
+    console.log(manager.office);
+    const managerPage = generateManagerPage(manager);
+    fs.writeFile('./dist/index.html', managerPage,(err) => 
+    err ? console.log(err) : console.log('Yay!')
+    );
+    userChoices();
+    })
+};
+
+
+// 3 separate fuctions to return 3 Strings
+// generatemanager card, index0
+// generate engineer and intern
+// break html into sections
+// Concat html
+
+
+
+
+function imDone(){
+    fs.appendFile("./dist/index.html", lastHtml,(err) => 
+    err ? console.log(err) : console.log('Yay!')
+);
+}
+
+
+const lastHtml =`</body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </html>
     
-    `
-};
+    `;
+
+
+
+
+
+
+
+
+// function generateHTML(data, name, id, email, github, school, oNumber) {
+
+//     const html =
+//     // create the card dinamically
+// <div class="card" style="width: 18rem;">
+//     <div class="card-body">
+//         <h5 class="card-title"></h5>
+//         <p class="card-text"></p>
+//     </div>
+//     <ul class="list-group list-group-flush">
+//         <li class="list-group-item">An item</li>
+//         <li class="list-group-item">A second item</li>
+//         <li class="list-group-item">A third item</li>
+//     </ul>
+// </div>
+
+// };
 
 
 
@@ -221,9 +282,8 @@ function generateHTML(data, name, id, email, github, school, oNumber) {
 // Start
 init();
 
-function createFile (fileName, data) {
-
-    fs.writeFile(fileName, `${data}`, (err) => {
-    err ? console.log(err) : console.log("It's Alive!!!")
-    })
-};
+// function createFile (fileName, data) {
+//     fs.writeFile(./ (err) => {
+//     err ? console.log(err) : console.log("It's Alive!!!")
+//     })
+// };
